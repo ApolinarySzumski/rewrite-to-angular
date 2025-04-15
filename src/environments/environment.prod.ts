6b9863f94839735c94977af8c8c15e1c69eb8d07
ApolinarySzumski <apolinary@poczta.onet.pl>
2025-04-15 14:15:34 +0200

    authorization is fixed

--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: true,
-    apiKey: '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'
-};
--- src/services/httpClient/http-client.service.ts
+++ src/services/httpClient/http-client.service.ts
@@ -1,18 +1,15 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { environment } from '../../environments/environment';
 
-const key = environment.apiKey
 @Injectable({
   providedIn: 'root'
 })
-
 export class HttpClientService {
   API_URL = "https://api.github.com"
   readonly http: HttpClient = inject(HttpClient);
   httpOptions: { headers: HttpHeaders } = {
     headers: new HttpHeaders({
-      Authorization: `Bearer ${key}`,
+      Authorization: 'Bearer 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',
       Accept: 'application/vnd.github.v3+json',
     }),
   }
