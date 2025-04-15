64526a495969d26ba9282f975df1a185d12e7787
ApolinarySzumski <apolinary@poczta.onet.pl>
2025-04-15 14:30:11 +0200

    check

--- src/services/httpClient/http-client.service.ts
+++ src/services/httpClient/http-client.service.ts
@@ -4,12 +4,4 @@ import { inject, Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class HttpClientService {
-  API_URL = "https://api.github.com"
-  readonly http: HttpClient = inject(HttpClient);
-  httpOptions: { headers: HttpHeaders } = {
-    headers: new HttpHeaders({
-      Accept: 'application/vnd.github.v3+json',
-    }),
-  }
-}
+
